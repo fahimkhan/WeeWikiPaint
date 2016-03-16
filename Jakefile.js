31f9616a44ed3267a4685732798c7206ea439513
fahimkhan <khanfahim2000@yahoo.com>
2016-03-16 18:41:21 +0530

    E2-Completed

--- Jakefile.js
+++ Jakefile.js
@@ -1,3 +1,6 @@
+/* globals desc ,task,jake,fail,complete */
+
+"use strict";
 
 task("default",["lint"]);
 
@@ -5,5 +8,16 @@ desc("Lint everything");
 task("lint",[],function(){
 	console.log("Lint Code goes here");
 	var lint = require("./build/lint/lint_runner.js");
-	lint.validateFile("Jakefile.js",{},{});
+
+	var files = new jake.FileList();
+	files.include("**/*.js");
+	files.exclude("node_modules");
+	files.exclude("build");
+
+	var options = {
+		node : true
+
+	};
+
+	lint.validateFileList(files.toArray(),options,{});
 });
