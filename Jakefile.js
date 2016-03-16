9bdaadd48e6c2310ac5b21a08eea2a9a2e70392a
fahimkhan <khanfahim2000@yahoo.com>
2016-03-16 14:19:43 +0530

    Finished Automation build

--- Jakefile.js
+++ Jakefile.js
@@ -1,8 +1,15 @@
+
+desc("Default Task")
+task("default",[],function(){
+	console.log("Default Task!!!");
+});
+
 desc("Example!!!!!");
 task("example",["dependency"],function(){
 	console.log("Example Task!!!");
 });
 
+desc("Dpendency")
 task("dependency",function(){
 	console.log("dependency");
 });
