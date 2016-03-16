6a8d74ee51984e05f50e89eed93374564f790ec7
fahimkhan <khanfahim2000@yahoo.com>
2016-03-16 23:28:20 +0530

    First runing test

--- Jakefile.js
+++ Jakefile.js
@@ -19,7 +19,7 @@
 	desc("Test Everything");
 	task("test",[],function(){
 		var reporter = require("nodeunit").reporters["default"];
-		reporter.run(['test']);
+		reporter.run(['src/server/_server_test.js']);
 	});
 
 
